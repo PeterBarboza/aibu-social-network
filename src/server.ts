8693923ebf574d43089189105ac8a8e8fa107d94
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-05 20:44:07 -0300

    TODO note added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { app, port } from "./app"
 import { connectToMongoDB } from "./database"
 
 app.listen(port, () => {
+  //TODO: Substituir logs por um logger de produção
   console.log(`Server is running on http://localhost:${port}`)
 
   connectToMongoDB()
