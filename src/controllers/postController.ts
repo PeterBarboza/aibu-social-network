9938a3e5b5bdbf0f2165cec0787075c9402758fa
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-05 20:56:08 -0300

    getPostsController created

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,9 +1,16 @@
 import { Request } from "express";
 
 import { createPostService } from "../services/post/createPostService"
+import { getPostsService } from "../services/post/getPostsService"
 
 export async function createPostController(req: Request) {
   const result = await createPostService(req)
+
+  return result
+}
+
+export async function getPostsController(req: Request) {
+  const result = await getPostsService(req)
 
   return result
 }
