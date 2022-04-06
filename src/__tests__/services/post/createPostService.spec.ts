5a9c4ab00848b09654d27d477f78b1f689b25fe2
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-06 12:58:04 -0300

    createPostService test created

---
+++ src/__tests__/services/post/createPostService.spec.ts
@@ -0,0 +1,67 @@
+import { Request } from "express"
+
+import { createPostService } from "../../../services/post/createPostService"
+import { Post } from "../../../models/post"
+import { User } from "../../../models/user"
+
+import mock from "../../mock/post.json"
+
+describe("Create post service", () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe("Happy path", () => {
+    it("Should return post with status 200", async () => {
+      Post.create = jest.fn(() => Promise.resolve(mock.postFullData) as any)
+      User.findOne = jest.fn(() => Promise.resolve(true) as any)
+
+      const { data, status } = await createPostService(mock.createPostParam as unknown as Request)
+
+      const spyCreate = jest.spyOn(Post, "create")
+      const spyFindOne = jest.spyOn(User, "findOne")
+
+      expect(spyFindOne).toHaveBeenCalledTimes(1)
+      expect(spyCreate).toHaveBeenCalledTimes(1)
+
+      expect(data.post).toMatchObject(mock.postFullData)
+
+      expect(status).toBe(200)
+    })
+
+    describe("Unhappy path", () => {
+      it("Should return a 'Invalid user_id' message with status 400", async () => {
+        Post.create = jest.fn(() => Promise.resolve(mock.postFullData) as any)
+        User.findOne = jest.fn(() => Promise.resolve(null) as any)
+
+        const { data, status } = await createPostService(mock.createPostParam as unknown as Request)
+
+        const spyCreate = jest.spyOn(Post, "create")
+        const spyFindOne = jest.spyOn(User, "findOne")
+
+        expect(spyFindOne).toHaveBeenCalledTimes(1)
+        expect(spyCreate).toHaveBeenCalledTimes(0)
+
+        expect(data.message).toBe("Invalid user_id")
+
+        expect(status).toBe(400)
+      })
+
+      it("Should return a error object with status 400", async () => {
+        Post.create = jest.fn(() => Promise.reject(mock.errorResponse.data) as any)
+        User.findOne = jest.fn(() => Promise.resolve(true) as any)
+
+        const { data, status } = await createPostService(mock.createPostParam as unknown as Request)
+
+        const spyCreate = jest.spyOn(Post, "create")
+        const spyFindOne = jest.spyOn(User, "findOne")
+
+        expect(spyFindOne).toHaveBeenCalledTimes(1)
+        expect(spyCreate).rejects.toBe(mock.errorResponse.data)
+
+        expect(data)
+        expect(status).toBe(400)
+      })
+    })
+  })
+})
