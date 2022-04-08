2292d67a9f0f89b91c4697b743bdffc5006dba01
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-08 18:41:08 -0300

    create comment service created

---
+++ src/__tests__/controllers/commentController.spec.ts
@@ -0,0 +1,46 @@
+import { Request } from "express"
+
+import { createLikeController } from "../../controllers/commentController"
+const { createCommentService } = require("../../services/comment/createCommentService")
+
+import mock from "../mock/comment.json"
+
+jest.mock("../../services/comment/createCommentService")
+
+describe("User controller", () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe("Happy path", () => {
+    beforeAll(() => {
+      createCommentService.mockImplementation(() => {
+        return Promise.resolve(mock.successCreateCommentResponse)
+      })
+    })
+
+    it("Create comment service: Should return a success object and status 200", async () => {
+      const { data, status } = await createLikeController(mock.createCommentParam.success as unknown as Request)
+
+      expect(data).toMatchObject(mock.successCreateCommentResponse.data)
+
+      expect(status).toBe(200)
+    })
+  })
+
+  describe("Unhappy path", () => {
+    beforeAll(() => {
+      createCommentService.mockImplementation(() => {
+        return Promise.resolve(mock.errorResponse)
+      })
+    })
+
+    it("Create post service: Should return a error object and status 400", async () => {
+      const { data, status } = await createCommentService(mock as unknown as Request)
+
+      expect(data.message)
+
+      expect(status).toBe(400)
+    })
+  })
+})
