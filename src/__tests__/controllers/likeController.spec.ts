6c5ce70a0349d7e35eb58c21bb1996245b451545
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-08 15:30:25 -0300

    likeController tests created

---
+++ src/__tests__/controllers/likeController.spec.ts
@@ -0,0 +1,46 @@
+import { Request } from "express"
+
+import { createLikeController } from "../../controllers/likeController"
+const { createLikeService } = require("../../services/like/createLikeService")
+
+import mock from "../mock/like.json"
+
+jest.mock("../../services/like/createLikeService")
+
+describe("Like controller", () => {
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe("Happy path", () => {
+    beforeAll(() => {
+      createLikeService.mockImplementation(() => {
+        return Promise.resolve(mock.successReponse)
+      })
+    })
+
+    it("Create like service: Should return a success object and status 200", async () => {
+      const { data, status } = await createLikeController(mock as unknown as Request)
+
+      expect(data.like).toMatchObject(mock.likeFullData)
+
+      expect(status).toBe(200)
+    })
+  })
+
+  describe("Unhappy path", () => {
+    beforeAll(() => {
+      createLikeService.mockImplementation(() => {
+        return Promise.resolve(mock.errorResponse)
+      })
+    })
+
+    it("Create like service: Should return a success object and status 200", async () => {
+      const { data, status } = await createLikeController(mock as unknown as Request)
+
+      expect(data.message)
+
+      expect(status).toBe(400)
+    })
+  })
+})
