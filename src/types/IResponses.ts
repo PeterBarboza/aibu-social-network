1d11aa5c5c544fbc5e4323235d437f13e3231003
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-10 18:18:57 -0300

    unused types removed

--- src/types/IResponses.ts
+++ src/types/IResponses.ts
@@ -1,29 +1,3 @@
-import { IUser } from "./IUser"
-import { IPost } from "./IPost"
-import { ILike } from "./ILike"
-
-//TODO: Apagar as interfaces de resposta caso não use nenhuma
-export interface IErrorMessageResponse {
-  message: any
-}
-
-export interface ISuccessUserResponse {
-  user: IUser
-  token: string
-}
-
-export interface ISuccessCreatePostResponse {
-  post: IPost
-}
-export interface ISuccessCreateLikeResponse {
-  like: ILike
-}
-
-export interface ISuccessGetPostsResponse {
-  posts: IPost[]
-  next: string
-}
-
 export interface IResponseData {
   status: number
   data: any
