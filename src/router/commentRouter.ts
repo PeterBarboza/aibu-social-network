f4e86907b4b58e10db192da4cfd1b61ee9dc51d6
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-10 18:22:11 -0300

    getComment route added

--- src/router/commentRouter.ts
+++ src/router/commentRouter.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
-import { createLikeController } from "../controllers/commentController"
+import { createCommentController, getCommentsController } from "../controllers/commentController"
 
 const commentRouter = Router()
 
 commentRouter.post("/create", async (req, res) => {
-  const { status, data } = await createLikeController(req)
+  const { status, data } = await createCommentController(req)
+
+  res.status(status).json(data)
+})
+commentRouter.get("/getComments", async (req, res) => {
+  const { status, data } = await getCommentsController(req)
 
   res.status(status).json(data)
 })
