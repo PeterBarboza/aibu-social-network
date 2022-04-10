9f14a873f63c00f3fe8a7db8154411da0ab3234d
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-10 18:22:40 -0300

    createdAt field added to schema

--- src/models/like.ts
+++ src/models/like.ts
@@ -3,6 +3,10 @@ import { Schema, model } from "mongoose"
 import { ILike } from "../types/ILike"
 
 const likeSchema = new Schema<ILike>({
+  createdAt: {
+    type: Number,
+    required: true
+  },
   post_id: {
     type: String,
     required: true
