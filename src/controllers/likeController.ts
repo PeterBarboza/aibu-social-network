56d63130b5873bee20a44c6f14efdbe8e36bf66a
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-10 19:03:43 -0300

    getLikesController added

--- src/controllers/likeController.ts
+++ src/controllers/likeController.ts
@@ -1,9 +1,16 @@
 import { Request } from "express";
 
 import { createLikeService } from "../services/postData/like/createLikeService"
+import { getLikesService } from "../services/postData/like/getLikesService"
 
 export async function createLikeController(req: Request) {
   const result = await createLikeService(req)
+
+  return result
+}
+
+export async function getLikesController(req: Request) {
+  const result = await getLikesService(req)
 
   return result
 }
