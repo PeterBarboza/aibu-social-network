cda3fbc026f5dbb7579f9f80a381d21d92e80976
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-10 18:24:58 -0300

    getCommentsController created

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,9 +1,16 @@
 import { Request } from "express";
 
 import { createCommentService } from "../services/postData/comment/createCommentService"
+import { getCommentsService } from "../services/postData/comment/getCommentsService"
 
-export async function createLikeController(req: Request) {
+export async function createCommentController(req: Request) {
   const result = await createCommentService(req)
+
+  return result
+}
+
+export async function getCommentsController(req: Request) {
+  const result = await getCommentsService(req)
 
   return result
 }
