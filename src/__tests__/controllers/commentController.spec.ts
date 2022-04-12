81c79ee62bee342b85c7ba7ba8e758f66133fd06
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-11 23:30:01 -0300

    getCommentsController tests created

--- src/__tests__/controllers/commentController.spec.ts
+++ src/__tests__/controllers/commentController.spec.ts
@@ -1,11 +1,13 @@
 import { Request } from "express"
 
-import { createCommentController } from "../../controllers/commentController"
+import { createCommentController, getCommentsController } from "../../controllers/commentController"
 const { createCommentService } = require("../../services/comment/createCommentService")
+const { getCommentsService } = require("../../services/comment/getCommentsService")
 
 import mock from "../mock/comment.json"
 
 jest.mock("../../services/comment/createCommentService")
+jest.mock("../../services/comment/getCommentsService")
 
 describe("User controller", () => {
   afterEach(() => {
@@ -17,12 +19,23 @@ describe("User controller", () => {
       createCommentService.mockImplementation(() => {
         return Promise.resolve(mock.successCreateCommentResponse)
       })
+      getCommentsService.mockImplementation(() => {
+        return Promise.resolve(mock.successgetCommentsResponse10)
+      })
     })
 
     it("Create comment service: Should return a success object and status 200", async () => {
-      const { data, status } = await createCommentController(mock.createCommentParam.success as unknown as Request)
+      const { data, status } = await createCommentController(mock as unknown as Request)
 
       expect(data).toMatchObject(mock.successCreateCommentResponse.data)
+
+      expect(status).toBe(200)
+    })
+
+    it("Get comments service: Should return a success object and status 200", async () => {
+      const { data, status } = await getCommentsController(mock as unknown as Request)
+
+      expect(data.comments).toMatchObject(mock.successgetCommentsResponse10.data.comments)
 
       expect(status).toBe(200)
     })
@@ -33,10 +46,21 @@ describe("User controller", () => {
       createCommentService.mockImplementation(() => {
         return Promise.resolve(mock.errorResponse)
       })
+      getCommentsService.mockImplementation(() => {
+        return Promise.resolve(mock.errorResponse)
+      })
     })
 
     it("Create post service: Should return a error object and status 400", async () => {
       const { data, status } = await createCommentController(mock as unknown as Request)
+
+      expect(data.message)
+
+      expect(status).toBe(400)
+    })
+
+    it("Get comments service: Should return a success object and status 200", async () => {
+      const { data, status } = await getCommentsController(mock as unknown as Request)
 
       expect(data.message)
 
