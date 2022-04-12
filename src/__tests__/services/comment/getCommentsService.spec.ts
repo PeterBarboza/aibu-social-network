cb66e438aff56df60d964343baa7e2c78e997b80
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-11 23:28:09 -0300

    tests created

--- src/__tests__/services/comment/getCommentsService.spec.ts
+++ src/__tests__/services/comment/getCommentsService.spec.ts
@@ -12,4 +12,226 @@ describe("Get comments service", () => {
     jest.clearAllMocks()
   })
 
+  const apiUrl = process.env.API_URL
+
+  describe("Happy path", () => {
+    const {
+      withUnixDateAndLimit,
+      withUnixDateAndWrongLimit,
+      withUnixDate,
+      withLimit,
+      empty } = mock.getCommentsParams.success
+
+    it("unixDate and limit on param: Should return a array with 5 comments and next url with status 200", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          lt: (param: any) => {
+            return {
+              sort: (param: any) => {
+                return {
+                  limit: () => Promise.resolve(mock.commentsArray5)
+                }
+              }
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(withUnixDateAndLimit as unknown as Request)
+
+      const expectedNextUrl = `${apiUrl}/comment/getComments?unixDate=${mock.commentsArray5[4].createdAt}&limit=${withUnixDateAndLimit.query.limit}`
+
+      expect(data.comments).toMatchObject(mock.commentsArray5)
+      expect(data.comments.length).toBe(5)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+
+    it("unixDate and limit on param: Should return a array with 4 comments and next filled with 'finish' and status 200", async () => {
+      const mockCommentsArray4 = mock.commentsArray5.map((item) => item)
+      mockCommentsArray4.pop()
+
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          lt: (param: any) => {
+            return {
+              sort: (param: any) => {
+                return {
+                  limit: () => Promise.resolve(mockCommentsArray4)
+                }
+              }
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(withUnixDateAndLimit as unknown as Request)
+
+      const expectedNextUrl = "finish"
+
+      expect(data.comments).toMatchObject(mockCommentsArray4)
+      expect(data.comments.length).toBe(4)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+
+    it("unixDate and wrong limit on param: Should return a array with 10 comments and next url with status 200", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          lt: (param: any) => {
+            return {
+              sort: (param: any) => {
+                return {
+                  limit: () => Promise.resolve(mock.commentsArray10)
+                }
+              }
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(withUnixDateAndWrongLimit as unknown as Request)
+
+      const expectedNextUrl = `${apiUrl}/comment/getComments?unixDate=${mock.commentsArray10[9].createdAt}&limit=${10}`
+
+      expect(data.comments).toMatchObject(mock.commentsArray10)
+      expect(data.comments.length).toBe(10)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+
+    it("Only unixDate on param: Should return a array with 10 comments and next url with status 200", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          lt: (param: any) => {
+            return {
+              sort: (param: any) => {
+                return {
+                  limit: () => Promise.resolve(mock.commentsArray10)
+                }
+              }
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(withUnixDate as unknown as Request)
+
+      const expectedNextUrl = `${apiUrl}/comment/getComments?unixDate=${mock.commentsArray10[9].createdAt}&limit=${10}`
+
+      expect(data.comments).toMatchObject(mock.commentsArray10)
+      expect(data.comments.length).toBe(10)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+
+    it("Only limit on param: Should return a array with 5 comments and next url with status 200", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          sort: (param: any) => {
+            return {
+              limit: () => Promise.resolve(mock.commentsArray5)
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(withLimit as unknown as Request)
+
+      const expectedNextUrl = `${apiUrl}/comment/getComments?unixDate=${mock.commentsArray5[4].createdAt}&limit=${withLimit.query.limit}`
+
+      expect(data.comments).toMatchObject(mock.commentsArray5)
+      expect(data.comments.length).toBe(5)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+
+    it("Empty param: Should return a array with 5 posts and next filled with 'finish' with status 200", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          sort: (param: any) => {
+            return {
+              limit: () => Promise.resolve(mock.commentsArray5)
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(empty as unknown as Request)
+
+      const expectedNextUrl = "finish"
+
+      expect(data.comments).toMatchObject(mock.commentsArray5)
+      expect(data.comments.length).toBe(5)
+      expect(data.next).toBe(expectedNextUrl)
+
+      expect(status).toBe(200)
+    })
+  })
+
+  describe("Unhappy path", () => {
+    const {
+      wrongUnixDateLengthAndLimit,
+      wrongUnixDateTypeAndLimit } = mock.getCommentsParams.fail
+
+    it("Wrong unixDate length and limit: Should return 'unixData format invalid. It must be 13 characters long' message with status 400", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+
+      const { data, status } = await getCommentsService(wrongUnixDateLengthAndLimit as unknown as Request)
+
+      expect(data.message).toBe("unixData format invalid. It must be 13 characters long")
+      expect(status).toBe(400)
+    })
+
+    it("Wrong unixDate type and limit: Should return 'unixData format invalid. It must contain numbers only' message with status 400", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+
+      const { data, status } = await getCommentsService(wrongUnixDateTypeAndLimit as unknown as Request)
+
+      expect(data.message).toBe("unixData format invalid. It must contain numbers only")
+      expect(status).toBe(400)
+    })
+
+    it("Any param: Should return a error object with status 400", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(false) as any)
+
+      const { data, status } = await getCommentsService(mock.getCommentsParams.success.withUnixDateAndLimit as unknown as Request)
+
+      expect(data.message).toBe("Invalid post_id")
+      expect(status).toBe(400)
+    })
+
+    it("Any param: Should return a error object with status 400", async () => {
+      Post.findOne = jest.fn(() => Promise.resolve(true) as any)
+      Comment.find = jest.fn(() => {
+        return {
+          lt: (param: any) => {
+            return {
+              sort: (param: any) => {
+                return {
+                  limit: (param: any) => Promise.reject(mock.errorResponse.data)
+                }
+              }
+            }
+          }
+        }
+      }) as any
+
+      const { data, status } = await getCommentsService(mock.getCommentsParams.success.withUnixDateAndLimit as unknown as Request)
+
+      expect(data.message)
+      expect(status).toBe(400)
+    })
+  })
 })
