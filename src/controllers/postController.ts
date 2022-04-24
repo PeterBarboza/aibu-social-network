0f9f26271ae7d638e96eded58a2e434e402891cf
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-24 05:14:35 -0300

    updateCommentController added

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -2,6 +2,7 @@ import { Request } from "express";
 
 import { createPostService } from "../services/post/createPostService"
 import { getPostsService } from "../services/post/getPostsService"
+import { updatePostService } from "../services/post/updatePostService"
 
 export async function createPostController(req: Request) {
   const result = await createPostService(req)
@@ -11,6 +12,12 @@ export async function createPostController(req: Request) {
 
 export async function getPostsController(req: Request) {
   const result = await getPostsService(req)
+
+  return result
+}
+
+export async function updateCommentController(req: Request) {
+  const result = await updatePostService(req)
 
   return result
 }
