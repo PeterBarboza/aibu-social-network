b3863f5ad10b494dc1bd20cf54bd55a8f29fc24b
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-30 14:20:20 -0300

    updatePostController name corrected; deletePostController added

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -3,6 +3,7 @@ import { Request } from "express";
 import { createPostService } from "../services/post/createPostService"
 import { getPostsService } from "../services/post/getPostsService"
 import { updatePostService } from "../services/post/updatePostService"
+import { deletePostService } from "../services/post/deletePostService"
 
 export async function createPostController(req: Request) {
   const result = await createPostService(req)
@@ -16,8 +17,14 @@ export async function getPostsController(req: Request) {
   return result
 }
 
-export async function updateCommentController(req: Request) {
+export async function updatePostController(req: Request) {
   const result = await updatePostService(req)
+
+  return result
+}
+
+export async function deletePostController(req: Request) {
+  const result = await deletePostService(req)
 
   return result
 }
