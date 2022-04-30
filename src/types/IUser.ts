bdbdbd90a190aa2a8ffc036ce04173d2b07401e3
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-30 13:48:40 -0300

    username and bio added to IUser; IUpdateUserReqBody and IUpdatePasswordReqBody created

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -2,7 +2,19 @@ export interface IUser {
   name: string
   email: string
   password: string
+  username: string
+  bio: string
   createdAt: number
+}
+
+export interface IUpdatePasswordReqBody {
+  newPassword: string
+}
+
+export interface IUpdateUserReqBody {
+  name: string
+  username: string
+  bio: string
 }
 
 export interface IUserLogin {
