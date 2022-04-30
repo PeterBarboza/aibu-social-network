6c1a42397fdea87250f5a82cd16f108074f3dd03
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-30 14:19:29 -0300

    update, delete and update user controllers added

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,9 @@ import { Request } from "express"
 
 import { createUserService } from "../services/user/createUserService"
 import { authUserService } from "../services/user/authUserService"
+import { updateUserService } from "../services/user/updateUserService"
+import { deleteUserService } from "../services/user/deleteUserService"
+import { updatePasswordService } from "../services/user/updatePasswordService"
 
 export async function createUserController(req: Request) {
   const result = await createUserService(req)
@@ -11,6 +14,28 @@ export async function createUserController(req: Request) {
 
 export async function authUserController(req: Request) {
   const result = await authUserService(req)
+
+  return result
+}
+
+export async function updateUserController(req: Request) {
+  const result = await updateUserService(req)
+
+  return result
+}
+
+export async function deleteUserController(req: Request) {
+  const { data, status } = await authUserService(req)
+
+  const result = await deleteUserService(req, status, data.user?._id)
+
+  return result
+}
+
+export async function updatePasswordController(req: Request) {
+  const { data, status } = await authUserService(req)
+
+  const result = await updatePasswordService(req, status, data.user?._id)
 
   return result
 }
