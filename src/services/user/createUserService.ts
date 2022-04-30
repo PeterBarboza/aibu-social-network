078e13d9504bb0d14592edd4fb89942ad02f92c7
PeterBarboza <pedrom.barboza01@gmail.com>
2022-04-30 13:55:15 -0300

    new verifications added

--- src/services/user/createUserService.ts
+++ src/services/user/createUserService.ts
@@ -1,4 +1,5 @@
 import { Request } from "express"
+import { hash } from "bcryptjs"
 
 import { User } from "../../models/user"
 
@@ -7,9 +8,51 @@ import { IResponseData } from "../../types/IResponses"
 import { generateToken } from "../../utils/generateToken"
 
 export async function createUserService(req: Request): Promise<IResponseData> {
-  const { name, email, password }: IUser = req.body
+  const { name, email, password, bio, username }: IUser = req.body
 
   try {
+    if (!name || name.length < 1) {
+      return {
+        status: 400,
+        data: {
+          message: "name not provided"
+        }
+      }
+    }
+
+    if (!username || username.length < 1) {
+      return {
+        status: 400,
+        data: {
+          message: "username not provided"
+        }
+      }
+    }
+
+    //TODO:Adicionar verificação pra ver se o endereço de email é real
+    if (!email || email.length < 1) {
+      return {
+        status: 400,
+        data: {
+          message: "email not provided"
+        }
+      }
+    }
+
+    if (await User.findOne({ username: username })) {
+      return {
+        status: 400,
+        data: {
+          message: "username already in use"
+        }
+      }
+    }
+
+    /*
+    //TODO: Crie uma verificação para ver se o email existe ou não
+    if(email...) {...}
+    */
+
     if (await User.findOne({ email: email })) {
       return {
         status: 400,
@@ -23,10 +66,13 @@ export async function createUserService(req: Request): Promise<IResponseData> {
     //Mínimo de X caracteres; as senhas devem conter pelo menos
     //X maiúsculas e X números ou caracteres especiais
 
+    const safePassword = await hash(password, 10)
     const user = await User.create({
       name: name,
       email: email,
-      password: password,
+      password: safePassword,
+      username: username,
+      bio: bio || "",
       createdAt: Date.now()
     })
 
