a19638e2b969ae8d4b74e063555d1fa8eb97b47b
Pedro Barboza <pedro.barboza.dev@gmail.com>
2022-06-28 06:10:48 -0300

    TODO note updated

--- src/useCases/user/getUsersProfilesService.ts
+++ src/useCases/user/getUsersProfilesService.ts
@@ -10,18 +10,22 @@ export async function getUsersProfilesService(req: Request): Promise<IResponseDa
   // _id[]=s76fds97asgq&_id[]=s76fds9asdhy&_id[]=s76fds9asdhy&_id[]=s76fds9asdhy&_id[]=s76fds9asdhy&_id[]=s76fds9asdhy
 
   //É a função abaixo quue ira gerar a querystring no frontend
-  /* const arr = ["a", "b", "c", "d", "e", "f"]
+  /* 
+  const arr = ["a", "b", "c", "d", "e", "f"]
 
   let query = ""
 
   arr.forEach((item, index, array) => {
-    if((index + 1) == array.length) {
-      query += `_id[]=${item}`
-      return
-    }
-
+    if(item) {
+      if((index + 1) == array.length) {
+        query += `_id[]=${item}`
+        return
+      }
+  
       query += `_id[]=${item}&`
-  }) */
+    }
+  }) 
+  */
 
   try {
     const profilesArray = await User.find({ _id: users_id })
